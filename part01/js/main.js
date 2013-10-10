f4a2168fc8b3b3a0ca169255cc35a25ed147d271
mbeatini <massimo.beatini@gmail.com>
2013-10-10 17:12:35 +0200

    notification alert

--- part01/js/main.js
+++ part01/js/main.js
@@ -1,4 +1,11 @@
 var app = {
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
 
     findByName: function() {
         console.log('findByName');
@@ -13,10 +20,14 @@ var app = {
         });
     },
 
+
     initialize: function() {
+        var self = this;
         //this.store = new MemoryStore();
         //this.store = new LocalStorageStore();
-        this.store = new WebSqlStore();
+        this.store = new WebSqlStore(function(){
+            self.showAlert('Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
